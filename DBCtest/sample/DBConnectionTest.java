2eb12e57397cc286d03f94e11fbedbd7a28215b4
Cassian <Louliett@gmail.com>
2019-04-09 17:45:48 +0200

    running tests

---
+++ DBCtest/sample/DBConnectionTest.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBConnectionTest {
+
+
+    DBConnection dbc = new DBConnection();
+
+    @Test
+    public void getEmail() {
+        dbc.getEmail();
+    }
+
+}
