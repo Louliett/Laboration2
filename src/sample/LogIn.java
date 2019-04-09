d74f4d21e757e9563a21cbf34fe4ffe7a98368d7
szomer <suzannezomer@gmail.com>
2019-04-09 17:03:40 +0200

    login screen

---
+++ src/sample/LogIn.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogIn implements Initializable {
+
+    @FXML Button logInButton, createAccountButton;
+    @FXML TextField emailField, passField;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void logInClick(){
+
+    }
+
+}
